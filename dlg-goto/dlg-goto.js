e6a3d717e2341aa328fc3163c3a2ede2e1bd4db5
wangding <408542507@qq.com>
2019-04-10 16:23:53 +0800

    转到对话框

--- dlg-goto/dlg-goto.js
+++ dlg-goto/dlg-goto.js
@@ -1,13 +1,35 @@
-/* exported $dlgAbout */
+/* exported $dlgGoto */
 var $dlgGoto = (function() {
-  function show() {
-      alert('hello goto dialog!');        
-  }
+    var DOM = ''
+        + '<div class="notepad-dlg-goto">'
+        + '<div class="dialogbox">'
+        + '<div class="titlebar">'
+        + '<p class="title">转到指定行</p>'
+        + '<span class="close-btn">✖</span>'
+        + '</div>'
+        + '<div class="main">'
+        + '<label for="">行号(L):</label><br>'
+        + '<input class="txt-line-num" type="text" autofocus><br>'
+        + '<input class="btn-goto" type="button" value="转到">'
+        + '<input class="btn-cancel" type="button" value="取消">'
+        + '</div>'
+        + '</div>'
+        + '</div>';
+    var $dlg = $(DOM),
+        $btnOk = $dlg.find('.btn-ok'),
+        $btnClose = $dlg.find('.close-btn'),
+        $btnCancel = $dlg.find('.btn-cancel');
 
-  return {show: show};
+    function show() {
+        $('body').append($dlg);
+        $dlg.find('.dialogbox').draggable({handle: $dlg.find('.titlebar')});
+
+        $btnOk.click(function() { $dlg.remove();  });
+        $btnClose.click(function() { $dlg.remove();  });
+        $btnCancel.click(function() { $dlg.remove();  });
+                          
+    }
+
+    return {show: show};
 
 })();
-
-$(function() {
-    $dlgGoto.show();
-});
